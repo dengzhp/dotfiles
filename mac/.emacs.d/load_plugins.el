cd2137ed127181d49b43f90ef0c7799cac97f9e6
freestyler <dengzhiping@corp.netease.com>
2013-01-09 18:42:48 +0800

    add hgignore/ install yasnippet using elpa

--- mac/.emacs.d/load_plugins.el
+++ mac/.emacs.d/load_plugins.el
@@ -23,8 +23,6 @@
  ;;      (define-key gtags-mode-map "\ev" 'gtags-visit-rootdir)
 ))
 
-(add-to-list 'load-path
-              "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/global-mode 1)
 
